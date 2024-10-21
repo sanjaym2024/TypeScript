5eb2c076f3263507b58f84c21f8f6a229885e812
Sanjay Mishra <sanjaym@maveric-systems.com>
2024-10-21 15:13:46 +0530

    'files-updates'

---
+++ dist/question1.js
@@ -0,0 +1,7 @@
+"use strict";
+// src/question1.ts
+function reverseString(str) {
+    return str.split('').reverse().join('') + ' - Updated';
+}
+console.log(reverseString("TypeScript"));
+console.log('sanjay');
---
+++ src/question1.ts
@@ -0,0 +1,8 @@
+// src/question1.ts
+function reverseString(str: string): string {
+    return str.split('').reverse().join('') + ' - Updated';
+  }
+  
+  console.log(reverseString("TypeScript"));
+  console.log('sanjay')
+  
